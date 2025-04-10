4198626dbccc50cf9fe3c5ca70b90298394e3915
Nziza.jpg <tayalinziza@gmail.com>
2025-04-10 18:54:33 +0200

    activities done

---
+++ activity 2/main.py
@@ -0,0 +1,10 @@
+# Print a sentence
+print("Welcome to the world of programming!")
+#Print a number
+print(4)
+#Next line after print
+print("Hello Python \n")
+#Print all the keywords present in python
+print("Keywords in python are....")
+import keyword
+print(keyword.kwlist)
