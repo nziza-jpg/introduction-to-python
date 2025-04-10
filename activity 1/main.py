8002d9639e5532b126293f8f3a9ed9144c6a9c0f
Nziza.jpg <tayalinziza@gmail.com>
2025-04-10 18:34:19 +0200

    activity done

---
+++ activity 1/main.py
@@ -0,0 +1,8 @@
+# Printing a greeting message
+print("Hello, world!")
+# Printing my name
+print("My name is Nziza.")
+# Printing a fun fact about myself
+print("I love playing sudoku")
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
